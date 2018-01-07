3481713e1c0640de219432d02af6ec7bcac88f6e
mohitkumaris <mohitkumaris@gmail.com>
2018-01-07 14:41:01 +0530

    .

--- server.js
+++ server.js
@@ -22,6 +22,55 @@ var port=process.env.PORT || 8090;
 
 var router=express.Router();
 
+ mongoose.connect('mongodb://localhost:27017/products');
+
+ router.use(function(req,res,next){
+
+
+     // do logging 
+    // do authentication 
+
+console.log("Logging of request will be done here");
+next();
+
+ });
+
+ //save the data
+router.route('/products').post(function(req,res){
+
+    var p = new product();
+    for(var i=0; i<req.body.length;i++){
+     p.title=req.body[i].title;
+     p.price=req.body[i].price;
+     p.instock=req.body[i].instock;
+     p.photo=req.body[i].photo;
+
+     p.save(function(err){
+
+        if(err)
+        {
+            res.send(err)
+        }
+
+        res.send({message:"Product Saved"});
+     })
+    }
+});
+
+router.route('/products').get(function(req,res){
+
+    product.find(function(err,products){
+       
+        if(err){
+            res.send(err)
+        }
+
+        res.send(products);
+
+    });
+
+});
+
 /*
 Use cors and assign base url,listen to port and start server
  */
